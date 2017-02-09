74512bb775c0d38c11ee19524f4bd776aa05f778
xuejun.wang <xuejun.wang@sinobbd.com>
2017-02-09 12:14:58 +0800

    commit avalon-cli

---
+++ README.md
@@ -0,0 +1,108 @@
+![](./components/logo.png)
+-------
+
+<p align="center">
+<a href="https://badge.fury.io/js/avalon2">
+<img src="https://badge.fury.io/js/avalon2.svg" alt="npm version" height="18">
+</a>
+<a href="https://travis-ci.org/RubyLouvre/avalon">
+<img src="https://travis-ci.org/RubyLouvre/avalon.svg?branch=master" alt="Travis CI Status"/>
+</a>
+<a href="https://codeclimate.com/github/RubyLouvre/avalon"><img src="https://codeclimate.com/github/RubyLouvre/avalon/badges/gpa.svg" /></a>
+
+<a href="https://codecov.io/gh/RubyLouvre/avalon">
+  <img src="https://codecov.io/gh/RubyLouvre/avalon/branch/master/graph/badge.svg" alt="Codecov" />
+</a>
+
+<a href="https://github.com/RubyLouvre/avalon">
+<img src="https://camo.githubusercontent.com/fc56303af12c023343f338a762b6bfb2a5f1e4dc/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6c6963656e73652d4d49542d677265656e2e7376673f7374796c653d666c6174" alt="LICENSE" data-canonical-src="https://img.shields.io/badge/license-MIT-green.svg?style=flat" style="max-width:100%;"></a>
+<a href="https://saucelabs.com/beta/builds/7c80ed0dd7b34c55815d65a132ff0226"><img src="https://saucelabs.com/buildstatus/rubylouvre" alt="Build Status"></a>
+</p>
+<p align="center">
+<a href="https://saucelabs.com/beta/builds/7c80ed0dd7b34c55815d65a132ff0226"><img src="https://saucelabs.com/browser-matrix/rubylouvre.svg" alt="Browser Matrix"></a>
+</p>
+
+```javascript
+npm install avalon2
+```
+
+es6调用方式 
+```
+import * as avalon from 'avalon2'
+```
+
+###[English tutorials](https://github.com/RubyLouvre/avalon/tree/master/tutorials)
+
+###[avalon cookbook](https://github.com/RubyLouvre/avalon/blob/2.1.8/avalon%20cookbook.pdf)
+
+这是基于官网制成的电子书,方便大家离线阅览
+
+###[avalon2官网](http://avalonjs.coding.me/)
+这是基于gitbook制作的,对移动端支持很好,大家上班或睡觉时,可以过目一下.
+
+###[avalon2+webpack2手脚架](https://github.com/sayll/avalon-webpack-start)
+
+###[QuickStart](https://github.com/crazyliuzc/webpack-avalon2-SPA-seed.git)
+大家期望已久的avalon2+mmRouter+各种组件的综合示例,不断完善中
+
+###[友情推广朋友写的UI库](http://weeksun23.github.io/AvalonBootstrap/index.html#overview)
+基于avalon2+bootstrap
+
+CDN: `https://unpkg.com/avalon2@2.2.4/dist/avalon.js`
+用法见[https://unpkg.com/#/](https://unpkg.com/#/)
+
+###谁在用avalon
+
+<img src='http://avalonjs.coding.me/styles/logos.jpg' width='639' height='477' />
+
+`欢迎大家提交logo与官网链接`
+
+
+###超高性能
+
+<img src="http://avalonjs.coding.me/styles/performance.jpg" width='770' height='451' />
+
+
+
+
+
+
+测试页面 perf目录下的index.html, index1.4.html, index-ng.html, index-vue.html,index-react.html
+
+亮点,如果页面上存在一个大表格或列表,其他框架会在浏览器加载页面时会卡一下(白屏), 
+而avalon则平缓多了
+
+thanks http://charts.udpwork.com/
+
+###支持后端渲染
+
+avalon2.1.5起支持后端渲染,详见[这里](https://github.com/RubyLouvre/avalon-server-render-example)
+
+
+### [avalon2 学习教程(包括组件)](https://segmentfault.com/u/situzhengmei/articles)
+
+
+HTML指南
+
+属性值必须用双引号括起,标签名必须小写, 标签必须被关闭（正常关闭，或自关闭）
+
+组件, 在兼容IE6-8的情况下,组件必须用wbr, xmp做容器
+
+组件名如果不充当标签名,可以不以ms-开头.
+
+绑定属性建议使用短指令方式定义,即ms-if可以改成:if
+
+ms-duplex指令除了change, rebounce过滤器外,不建议使用其他过滤器做格式化,
+建议添加ms-input, ms-change在里面处理
+
+ms-duplex不支持对简单数组的元素的处理, 即`vm.arr = [1,2,4]`,
+`<input :for="el in @arr" duplex="el"`
+
+
+ms-important与ms-controller对应的vm.$id一个页面上只能用一次,不能存在多个同名的ms-controller.
+     ms-important由于不继承上级的$element与$render,每次只更新它所在的区域,善用它能大大提高性能
+```html
+   <div ms-controller='test'>{{@aaa}}</div>
+   <div ms-controller='test'>{{@bbb}}<!--test已经使用了1次!会导致程序出错--></div>
+   <div ms-important='test'>{{@bbb}}<!--test已经使用了2次!会导致程序出错--></div>
+```
---
+++ src/directives/validate.js
@@ -0,0 +1,225 @@
+import { avalon, isObject, platform } from '../seed/core'
+var valiDir = avalon.directive('validate', {
+    diff: function(validator) {
+        var vdom = this.node
+        if (vdom.validator) {
+            return
+        }
+        if (isObject(validator)) {
+            //注意，这个Form标签的虚拟DOM有两个验证对象
+            //一个是vmValidator，它是用户VM上的那个原始子对象，也是一个VM
+            //一个是validator，它是vmValidator.$model， 这是为了防止IE6－8添加子属性时添加的hack
+            //也可以称之为safeValidate
+            vdom.vmValidator = validator
+            validator = platform.toJson(validator)
+            validator.vdom = vdom
+            vdom.validator = validator
+            for (var name in valiDir.defaults) {
+                if (!validator.hasOwnProperty(name)) {
+                    validator[name] = valiDir.defaults[name]
+                }
+            }
+            validator.fields = validator.fields || []
+            return true
+        }
+    },
+    update: function(vdom) {
+        
+        var validator = vdom.validator
+        var dom = validator.dom = vdom.dom
+        dom._ms_validate_ = validator
+        var fields = validator.fields
+        collectFeild(vdom.children, fields, validator)
+        avalon.bind(document, 'focusin', function(e) {
+            var dom = e.target
+            var duplex = dom._ms_duplex_
+            var vdom = (duplex || {}).vdom
+            if (duplex && vdom.rules && !duplex.validator) {
+                if (avalon.Array.ensure(fields, duplex)) {
+                    bindValidateEvent(duplex, validator)
+                }
+
+            }
+        })
+        
+        //为了方便用户手动执行验证，我们需要为原始vmValidate上添加一个onManual方法
+        var v = vdom.vmValidator
+        try {
+            v.onManual = onManual
+        } catch (e) {}
+        delete vdom.vmValidator
+
+        dom.setAttribute('novalidate', 'novalidate')
+
+        function onManual() {
+            valiDir.validateAll.call(validator, validator.onValidateAll)
+        }
+        /* istanbul ignore if */
+        if (validator.validateAllInSubmit) {
+            avalon.bind(dom, 'submit', function(e) {
+                e.preventDefault()
+                onManual()
+            })
+        }
+        
+    },
+    validateAll: function(callback) {
+        var validator = this
+        var vdom = this.vdom
+        var fields = validator.fields = []
+        collectFeild(vdom.children, fields, validator)
+        var fn = typeof callback === 'function' ? callback : validator.onValidateAll
+        var promises = validator.fields.filter(function(field) {
+            var el = field.dom
+            return el && !el.disabled && validator.dom.contains(el)
+        }).map(function(field) {
+            return valiDir.validate(field, true)
+        })
+        var uniq = {}
+        return Promise.all(promises).then(function(array) {
+            var reasons = array.concat.apply([], array)
+            if (validator.deduplicateInValidateAll) {
+
+                reasons = reasons.filter(function(reason) {
+                    var el = reason.element
+                    var uuid = el.uniqueID || (el.uniqueID = setTimeout('1'))
+
+                    if (uniq[uuid]) {
+                        return false
+                    } else {
+                        return uniq[uuid] = true
+                    }
+                })
+            }
+            fn.call(validator.dom, reasons) //这里只放置未通过验证的组件
+        })
+    },
+    
+    validate: function(field, isValidateAll, event) {
+        var promises = []
+        var value = field.value
+        var elem = field.dom
+
+        /* istanbul ignore if */
+        if (typeof Promise !== 'function') { //avalon-promise不支持phantomjs
+            avalon.warn('浏览器不支持原生Promise,请下载并<script src=url>引入\nhttps://github.com/RubyLouvre/avalon/blob/master/test/promise.js')
+        }
+        /* istanbul ignore if */
+        if (elem.disabled)
+            return
+        var rules = field.vdom.rules
+        var ngs = [],
+            isOk = true
+        if (!(rules.norequired && value === '')) {
+            for (var ruleName in rules) {
+                var ruleValue = rules[ruleName]
+                if (ruleValue === false)
+                    continue
+                var hook = avalon.validators[ruleName]
+                var resolve
+                promises.push(new Promise(function(a, b) {
+                    resolve = a
+                }))
+                var next = function(a) {
+                    var reason = {
+                        element: elem,
+                        data: field.data,
+                        message: elem.getAttribute('data-' + ruleName + '-message') || elem.getAttribute('data-message') || hook.message,
+                        validateRule: ruleName,
+                        getMessage: getMessage
+                    }
+                    if (a) {
+                        resolve(true)
+                    } else {
+                        isOk = false
+                        ngs.push(reason)
+                        resolve(false)
+                    }
+                }
+                field.data = {}
+                field.data[ruleName] = ruleValue
+                hook.get(value, field, next)
+            }
+        }
+
+        //如果promises不为空，说明经过验证拦截器
+        return Promise.all(promises).then(function(array) {
+            if (!isValidateAll) {
+                var validator = field.validator
+                if (isOk) {
+                    validator.onSuccess.call(elem, [{
+                        data: field.data,
+                        element: elem
+                    }], event)
+                } else {
+                    validator.onError.call(elem, ngs, event)
+                }
+                validator.onComplete.call(elem, ngs, event)
+            }
+            return ngs
+        })
+    }
+})
+
+function collectFeild(nodes, fields, validator) {
+    for (var i = 0, vdom; vdom = nodes[i++];) {
+        var duplex = vdom.rules && vdom.duplex
+        if (duplex) {
+            fields.push(duplex)
+            bindValidateEvent(duplex, validator)
+        } else if (vdom.children) {
+            collectFeild(vdom.children, fields, validator)
+        } else if (Array.isArray(vdom)) {
+            collectFeild(vdom, fields, validator)
+        }
+    }
+}
+
+function bindValidateEvent(field, validator) {
+
+    var node = field.dom
+    if (field.validator) {
+        return
+    }
+    field.validator = validator
+        /* istanbul ignore if */
+    if (validator.validateInKeyup && (!field.isChanged && !field.debounceTime)) {
+        avalon.bind(node, 'keyup', function(e) {
+            validator.validate(field, 0, e)
+        })
+    }
+    /* istanbul ignore if */
+    if (validator.validateInBlur) {
+        avalon.bind(node, 'blur', function(e) {
+            validator.validate(field, 0, e)
+        })
+    }
+    /* istanbul ignore if */
+    if (validator.resetInFocus) {
+        avalon.bind(node, 'focus', function(e) {
+            validator.onReset.call(node, e, field)
+        })
+    }
+}
+var rformat = /\\?{{([^{}]+)\}}/gm
+
+function getMessage() {
+    var data = this.data || {}
+    return this.message.replace(rformat, function(_, name) {
+        return data[name] == null ? '' : data[name]
+    })
+}
+valiDir.defaults = {
+    validate: valiDir.validate,
+    onError: avalon.noop,
+    onSuccess: avalon.noop,
+    onComplete: avalon.noop,
+    onManual: avalon.noop,
+    onReset: avalon.noop,
+    onValidateAll: avalon.noop,
+    validateInBlur: true, //@config {Boolean} true，在blur事件中进行验证,触发onSuccess, onError, onComplete回调
+    validateInKeyup: true, //@config {Boolean} true，在keyup事件中进行验证,触发onSuccess, onError, onComplete回调
+    validateAllInSubmit: true, //@config {Boolean} true，在submit事件中执行onValidateAll回调
+    resetInFocus: true, //@config {Boolean} true，在focus事件中执行onReset回调,
+    deduplicateInValidateAll: false //@config {Boolean} false，在validateAll回调中对reason数组根据元素节点进行去重
+}
